5e6e06b560161dc00afe46976ad711a27369ca29
Yannik Rödel <hey@yannik.info>
2022-04-07 15:19:01 +0200

    Add scan command and libraries migrations

---
+++ tumpara/libraries/management/commands/scan.py
@@ -0,0 +1,25 @@
+import logging
+from typing import Any
+
+from django.core import management
+
+from tumpara.libraries.models import Library
+
+_logger = logging.getLogger(__name__)
+
+
+class Command(management.BaseCommand):
+    help = "Run a full scan on all libraries."
+
+    def handle(self, *args: Any, **kwargs: Any) -> None:
+        library_count = Library.objects.count()
+        if library_count == 0:
+            _logger.warning("Could not start scan because no libraries exist.")
+            return
+        elif library_count == 1:
+            _logger.info(f"Starting consecutive scan of {library_count} library...")
+        else:
+            _logger.info(f"Starting consecutive scan of {library_count} libraries...")
+
+        for library in Library.objects.all():
+            library.scan()
