e68139c84c59952b7513be32cdd1beef636c574b
Yannik Rödel <hey@yannik.info>
2022-04-29 17:12:21 +0200

    Begin with records API

--- tumpara/libraries/api/records.py
+++ tumpara/libraries/api/records.py
@@ -10,7 +10,17 @@ from tumpara.accounts import models as accounts_models
 from tumpara.libraries import models as libraries_models
 from tumpara.libraries import storage
 
+from .libraries import LibraryNode, LibraryVisibility
+
+
+@strawberry.enum
+class RecordVisibility(LibraryVisibility):
+    INHERIT = libraries_models.Visibility.INHERIT
+
 
 @strawberry.interface(name="Record")
-class RecordNode(api.DjangoNode[libraries_models.Record]):
-    pass
+class RecordNode(
+    api.DjangoNode[libraries_models.Record], fields=["library", "visibility"]
+):
+    library: Optional[LibraryNode]
+    visibility: RecordVisibility
